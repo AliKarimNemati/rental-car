6ec052419f350c3a80bcc057b83b5d58ae65afd0
ali <alineamati.1381@gmail.com>
2022-11-02 20:31:59 +0330

    navbar created

---
+++ components/Navbar.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid d-flex">
+      <nuxt-link class="navbar-brand" to="/">RENTY</nuxt-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Service</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Cars</a>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <button type="button" class="btn btn-dark">Download app</button>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+    
+};
+</script>
+
+<style>
+.navbar-brand{
+    font-weight: 500;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <template>
-  <Tutorial/>
+<Navbar />
 </template>
 
 <script>
+import Navbar from '~/components/Navbar.vue'
 export default {
+  components: { Navbar },
   name: 'IndexPage'
 }
 </script>
