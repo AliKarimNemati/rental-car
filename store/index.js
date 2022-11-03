c877fe3c89c20f868fa71fd8ac1215ff062ea7cc
ali <alineamati.1381@gmail.com>
2022-11-03 13:35:50 +0330

    data added

---
+++ store/index.js
@@ -0,0 +1,129 @@
+export const state = () => ({
+   popularCars:  [
+    {
+        id: 1,
+        name : "Chevrolet Camaro 2016",
+        loc: "Paris",
+        price: 80,
+        img: "1029548-chevrolet-camaro-ss-convertible-cars-red-2016.jpg"
+    },
+    {
+        id: 2,
+        name : "Maserati Ghibli 2021",
+        loc: "Landon",
+        price: 85,
+        img: "maserati ghibli 2021.jpg"
+    },
+    {
+        id: 3,
+        name : "Mercedes Benz",
+        loc: "Denver",
+        price: 90,
+        img: "Mercedes Benz.jpg"
+    },
+    {
+        id: 4,
+        name : "Porsche 911GTXTarga PDK",
+        loc: "Landon",
+        price: 80,
+        img: "porsche 911GTXTarga PDK.jpg"
+    },
+    {
+        id: 5,
+        name : "Toyota Mirai Sedan 2017",
+        loc: "Paris",
+        price: 80,
+        img: "2017-Toyota-Mirai-Sedan-image-01.jpg"
+    },
+    {
+        id: 6,
+        name : "kia rio S",
+        loc: "Paris",
+        price: 70,
+        img: "kia rio S.jpg"
+    },
+],
+
+largeCars: [
+    {
+        id: 1,
+        name : "Kia Sorento 2017",
+        loc: "Paris",
+        price: 80,
+        img: "2017-Kia-Sorento-SUV-white-press-image-1001x565-best-large-family-cars-hero-image.jpg"
+    },
+    {
+        id: 2,
+        name : "Mazda cx5 2022",
+        loc: "Landon",
+        price: 85,
+        img: "2020-mazda-cx-5---uk-spec.jpg"
+    },
+    {
+        id: 3,
+        name : "Chevrolet equinox SUV",
+        loc: "Denver",
+        price: 90,
+        img: "chevrolet equinox SUV.jpg"
+    },
+    {
+        id: 4,
+        name : "Hyundai tucson SUV",
+        loc: "Landon",
+        price: 80,
+        img: "hyundai tucson SUV.jpg"
+    },
+    {
+        id: 5,
+        name : "Lincoln navigator",
+        loc: "Paris",
+        price: 80,
+        img: "lincoln navigator.jpg"
+    },
+    {
+        id: 6,
+        name : "Mercedes Benz GLS",
+        loc: "Paris",
+        price: 70,
+        img: "Mercedes Benz GLS.jpg"
+    },
+],
+
+smallCars:  [
+    {
+        id: 1,
+        name : "BMW i30 2018",
+        loc: "Paris",
+        price: 80,
+        img: "bmw i30 2018.jpg"
+    },
+    {
+        id: 2,
+        name : "Honda jazz 2020",
+        loc: "Landon",
+        price: 85,
+        img: "honda jazz 2020.jpg"
+    },
+    {
+        id: 3,
+        name : "Kia soul 2021",
+        loc: "Denver",
+        price: 90,
+        img: "kia soul 2021.jpg"
+    },
+    {
+        id: 4,
+        name : "Volkswagen Up",
+        loc: "Landon",
+        price: 80,
+        img: "volkswagen Up.jpg"
+    },
+    {
+        id: 5,
+        name : "Toyota aygo",
+        loc: "Paris",
+        price: 80,
+        img: "Toyota aygo.jpg"
+    }
+]
+});
